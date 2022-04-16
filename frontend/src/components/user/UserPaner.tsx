961e6384e0325515a57b6402e384d32700a0067a
vohoanglong0107 <mamlong34@gmail.com>
2022-04-16 22:14:44 +0700

    test

--- frontend/src/components/user/UserPaner.tsx
+++ frontend/src/components/user/UserPaner.tsx
@@ -1,6 +1,11 @@
 import { Typography } from "@mui/material";
+import User from "./User";
 
-export default function UserPaner() {
+interface IUserPanerProps {
+  user: User;
+}
+
+export default function UserPaner({ user }: IUserPanerProps) {
   return (
     <Typography variant="h6" color="inherit" noWrap>
       UserPaner
